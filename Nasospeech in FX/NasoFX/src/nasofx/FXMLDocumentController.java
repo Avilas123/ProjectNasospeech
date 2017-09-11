b035294508b3ff760c3458497d9a7dbdd94197d0
iitgnasospeech <erroshanchettri89@gmail.com>
2017-09-11 17:22:59 +0530

    Nasospeech in FX

---
+++ Nasospeech in FX/NasoFX/src/nasofx/FXMLDocumentController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nasofx;
+
+import java.awt.Image;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+
+/**
+ *
+ * @author IITG
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+       
+    }
+     
+    
+     @FXML
+    private SplitPane sp;
+
+    @FXML
+    private AnchorPane ap1;
+
+    @FXML
+    private AnchorPane ap2;
+    
+
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+       
+        
+    }    
+    
+}
