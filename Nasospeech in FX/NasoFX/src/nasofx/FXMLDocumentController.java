4e12bb0795889bbeaa5511508e8da5eb06d2751c
Avilas123 <avilas123@gmail.com>
2017-09-12 16:48:01 +0530

    new commit

--- Nasospeech in FX/NasoFX/src/nasofx/FXMLDocumentController.java
+++ Nasospeech in FX/NasoFX/src/nasofx/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package nasofx;
 
 import java.awt.Image;
+import java.awt.geom.GeneralPath;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
@@ -20,6 +22,8 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 /**
  *
@@ -33,6 +37,8 @@ public class FXMLDocumentController implements Initializable {
     private Tab tab1;
            @FXML
     private TabPane TP;
+ 
+
 
 
 
@@ -48,13 +54,26 @@ public class FXMLDocumentController implements Initializable {
     slider.setMin(0);
     slider.setMax(100);
     slider.setValue(40);
-    slider.setShowTickLabels(true);
+    //slider.setShowTickLabels(true);
     slider.setShowTickMarks(true);
     slider.setMajorTickUnit(50);
     slider.setMinorTickCount(5);
     slider.setBlockIncrement(10);
     
    TP.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
+   
+for (int i = 0; i <600; i+=10)
+{
+    Line line1 = new Line(i, 0, i, 600);
+    line1.setStroke(Color.LIGHTGRAY);
+    Line line2 = new Line(0, i, 600, i);
+    line2.setStroke(Color.LIGHTGRAY);
+    tab1.addAll(line1, line2);
+}
+
+
+   
+   
     
  
     }    
